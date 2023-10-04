c88a70648dcfd6f23e1abc71a2377e0311c36d9b
EstebanFTellezR <estebanf.tellezr@konradlorenz.edu.co>
2023-10-04 18:24:04 -0500

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,23 @@
+/**
+ * importar libreria de listas
+ */
+/**Complejidadtemporal:O(1)Tiempoconstante.
+ *
+ */
+
+public class Principal {
+    public static void main(String[] args) {
+        Biblioteca bibliotecaKonradLorenz = new Biblioteca();
+
+        Libro libro100AnosSoledad = new Libro("Cien años de Soledad", "Gabriel Garcia Marquez", 496);
+        bibliotecaKonradLorenz.registrarLibro(libro100AnosSoledad);
+        Libro libroElcodigoDaVinci = new Libro("ElcodigoDaVinci", "Dan Brown", 656);
+        bibliotecaKonradLorenz.registrarLibro(libroElcodigoDaVinci);
+        Libro milNovecientosOchentayCuatro = new Libro("1984", "GeorgeOrwell", 326);
+        bibliotecaKonradLorenz.registrarLibro(milNovecientosOchentayCuatro);
+        Libro elHobbit = new Libro("ElHobbit", "J.R.R.Tolkien", 310);
+        bibliotecaKonradLorenz.registrarLibro(elHobbit);
+        Libro laOdisea = new Libro("LaOdisea", "Homero", 448);
+        bibliotecaKonradLorenz.registrarLibro(laOdisea);
+    }
+}
